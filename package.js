a25d3d306778088b04992d987687bc2e4f288bba
Sven Buschbeck <sven.buschbeck@gmail.com>
2017-06-01 15:30:36 +0200

    Filled package.js's metadata for atmospherejs release

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'svub:mock-location',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'Manipulates the browser\'s geolocation API to mock locations for testing puposes.',
+  git: 'https://github.com/svub/meteor-mock-geolocation.git',
   documentation: 'README.md'
 });
 
